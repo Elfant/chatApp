ab5787c1a9bb23a0a245692972f0e7035930220c
Bartek Sadecki <barteksadecki@gmail.com>
2021-01-14 15:01:13 +0100

    Styled input and messages

--- client/components/Conversation.jsx
+++ client/components/Conversation.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import CreateMessage from "./CreateMessage";
+import Message from "./Message";
 
 const Conversation = () => (
   <div className="conversation">
+    <Message />
+    <Message />
+    <Message />
+    <Message />
+    <Message />
     <CreateMessage />
   </div>
 );
---
+++ client/components/Message.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Message = () => {
+  return(
+    <div className="conversation__message message">
+      <p>
+        Wiadomość ode mnie 1
+      </p>
+    </div>
+  );
+};
+
+export default Message;
